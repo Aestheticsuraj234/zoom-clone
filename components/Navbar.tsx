ca8444115e88a01b4d05fb3620469b02ea947195
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-03-31 11:12:40 +0530

    Add dependencies and update UI components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50 w-full bg-dark-1 py-4 lg:px-10">
+    <nav className="  flex-between fixed z-50 w-full bg-dark-1 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src={"/icons/logo.svg"}
@@ -20,8 +21,12 @@ const Navbar = () => {
         </p>
       </Link>
 
-      <div className="flex-between gap-5">
-        {/* Clerk - UserManagement */}
+      <div className=" flex-between gap-5">
+        
+<SignedIn>
+<UserButton />
+</SignedIn>
+
         <MobileNav />
       </div>
     </nav>
