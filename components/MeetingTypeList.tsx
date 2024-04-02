24f2c0bc0b9be03bb60326e9cd5ecb2a7bc8f122
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-04-02 12:13:07 +0530

    Add MeetingModal component and createMeeting function

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -2,19 +2,25 @@
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
+import { create } from "domain";
 
 const MeetingTypeList = () => {
   const [MeetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
   const router = useRouter();
+
+
+  const createMeeting = ()=>{}
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
@@ -37,6 +43,17 @@ const MeetingTypeList = () => {
         description="via invitation link"
         handleClick={() => setMeetingState("isJoiningMeeting")}
         className="bg-yellow-1"
+      />
+
+
+      <MeetingModal 
+      isOpen={MeetingState === "isInstantMeeting"}
+      onClose={() => setMeetingState(undefined)}
+      title="Start and Instant Meeting"
+      className="text-center"
+      buttonText="Start Meeting"
+      handleClick={createMeeting}
+      
       />
     </section>
   );
