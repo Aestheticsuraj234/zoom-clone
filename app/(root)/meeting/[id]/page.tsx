0f21bcccfec8fec44bf40dc300afa03c03ad3cd6
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-04-03 13:31:22 +0530

    Refactor meeting page and update toaster styling

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+"use client";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallbyId } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React from "react";
 
-const Meeting = (
-    {params}:{params:{id:string}}
-) => {
+const Meeting = ({ params }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+
+  const {call , isCallLoading} = useGetCallbyId(params.id);
+  const [isSetupComplete, setIsSetupComplete] = React.useState(false);
+
   return (
-    <div>Meeting</div>
-  )
-}
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup/> : <MeetingRoom/>}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
 
-export default Meeting
+export default Meeting;
---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useCall, VideoPreview } from '@stream-io/video-react-sdk'
+import React, { useEffect, useState } from 'react'
+
+const MeetingSetup = () => {// https://getstream.io/video/docs/react/ui-cookbook/replacing-call-controls/
+  const [isMicCamToggled, setIsMicCamToggled] = useState(false);
+  const call = useCall();
+
+  if (!call) {
+    throw new Error(
+      'useStreamCall must be used within a StreamCall component.',
+    );
+  }
+ useEffect(() => {
+    if (isMicCamToggled) {
+      call.camera.disable();
+      call.microphone.disable();
+    } else {
+      call.camera.enable();
+      call.microphone.enable();
+    }
+  }, [isMicCamToggled, call.camera, call.microphone]);
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-2xl font-bold ">
+        SetUp
+      </h1>
+      <VideoPreview />
+    </div>
+  )
+}
+
+export default MeetingSetup
